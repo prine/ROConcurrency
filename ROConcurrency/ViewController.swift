2a1d642598a268e480f19fc907558f4c4bd99899
Robin Oster <robin.oster@rascor.com>
2015-07-16 13:48:26 +0200

    Initial Commit

---
+++ ROConcurrency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ROConcurrency
+//
+//  Created by Robin Oster on 16/07/15.
+//  Copyright (c) 2015 Rascor International AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
